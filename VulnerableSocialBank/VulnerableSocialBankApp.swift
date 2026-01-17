0579987736c0d8aa5b3f6d23887969e49f557cc1
Prokofiev Andrii <prokofiev.main@gmail.com>
2026-01-17 17:06:23 +0100

    Initial Commit

---
+++ VulnerableSocialBank/VulnerableSocialBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  VulnerableSocialBankApp.swift
+//  VulnerableSocialBank
+//
+//  Created by Andrii Prokofiev on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct VulnerableSocialBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
